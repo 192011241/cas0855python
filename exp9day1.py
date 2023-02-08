2372517f6191b8a6c2a30d5abc10782dabbd9dd9
192011241 <112851388+192011241@users.noreply.github.com>
2023-02-08 14:15:03 +0530

    Add files via upload

---
+++ exp9day1.py
@@ -0,0 +1,21 @@
+t=int(input("value of t="))
+print("E[] =")
+a=[]
+b=[]
+c=[]
+
+for i in range (0,t):
+    a.append(int(input("")))
+print("l[] =")
+
+for i in range (0,t):
+    b.append(int(input("")))
+
+for i in range (0,t):
+    c.append(a[i]+b[i])
+
+x=0
+for i in range (0,t):
+    if(x<c[i]):
+        x=c[i]
+print("MAXIMUM IS ",x)
