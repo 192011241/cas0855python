68407b7d201c0f5cd92917bfaa75a01fc9ab39f0
192011241 <112851388+192011241@users.noreply.github.com>
2023-02-08 13:14:25 +0530

    Add files via upload

---
+++ exp5 day1.py
@@ -0,0 +1,6 @@
+x=float(input("enter the number of fresh bread purchased:"))
+y=float(input("enter the number of old day bread purchased:"))
+print("regular price:185.00")
+print("amount of fresh bread:",x*185.0)
+print("amount of old day bread:",y*185*0.4)
+print("total amount:",x*185.0+y*185*0.4)
